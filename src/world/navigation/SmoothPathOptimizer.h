9f8894c38de46614b53a26245b115736c4a0f043
Kostiantyn Zubchenko [FE-30] <90088507+forzub@users.noreply.github.com>
2026-09-15 21:44:15 +0300

    diagnostics: announce navigation perf log at startup

--- src/world/navigation/SmoothPathOptimizer.h
+++ src/world/navigation/SmoothPathOptimizer.h
@@ -9,6 +9,7 @@
 
 #include "src/world/navigation/NavigationObstacle.h"
 #include "src/world/navigation/NavigationVehicleProfile.h"
+#include "src/world/navigation/NavigationPerfLog.h"
 
 namespace world::navigation
 {

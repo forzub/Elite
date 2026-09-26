90f6ef2256396abbf0d47a75fdf660dcc177b9ed
Kostiantyn Zubchenko [FE-30] <90088507+forzub@users.noreply.github.com>
2026-09-26 14:36:55 +0300

    flight: use shared default law in client command latch

--- src/game/client/GameClient.h
+++ src/game/client/GameClient.h
@@ -208,7 +208,7 @@ private:
     bool                            m_hasPendingLocalControlLawCommand = false;
     game::navigation::LocalFlightControlLaw
                                     m_pendingLocalControlLaw =
-        game::navigation::LocalFlightControlLaw::Assisted;
+        game::navigation::defaultLocalFlightControlLaw();
     game::navigation::VelocityAlignmentMode
                                     m_pendingVelocityAlignmentCommand =
         game::navigation::VelocityAlignmentMode::None;
